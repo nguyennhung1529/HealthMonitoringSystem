a1383f01ba3b1c880d7161ae25dae1d68d90cb7a
nguyennhung1529 <nguyenthinhung1529@gmail.com>
2022-03-19 17:30:06 +0700

    upload from local
    upload from local

---
+++ app/src/main/java/com/example/healthmonitor/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.healthmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
