93942b112992598647a763383fbd180e731c0d28
donghang <donghangaatt@gmail.com>
2022-04-25 16:55:40 +0700

    giao diện footsteps.java

---
+++ app/src/main/java/com/example/healthmonitor/footsteps.java
@@ -0,0 +1,14 @@
+package com.example.healthmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class footsteps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_footsteps);
+    }
+}
